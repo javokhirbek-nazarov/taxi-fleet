ebd827a032bb63c6e856d04546023aedc6fa126a
Javokhirbek Nazarov <javokhirbek.nazarov.99@gmail.com>
2024-09-15 21:12:27 +0500

    fix jackson deserialize error because of missing no args constructor

--- taxi-fleet/src/main/java/booking/task/taxi_fleet/taxi/model/LocationDto.java
+++ taxi-fleet/src/main/java/booking/task/taxi_fleet/taxi/model/LocationDto.java
@@ -2,10 +2,12 @@ package booking.task.taxi_fleet.taxi.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class LocationDto {
 
